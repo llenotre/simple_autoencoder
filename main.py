21b16aa78e919dc55fa644b25b42602fcfa7567d
llenotre <llenotre@student.42.fr>
2021-08-17 13:05:39 +0200

    Fixed errors

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+#!/usr/bin/python3
+
+import sys
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
+from torchvision import transforms
 from torchvision.transforms import ToTensor, ToPILImage
 import torch.nn.functional as F
 
@@ -23,18 +27,17 @@ class ImageDataset(Dataset):
 
     def __getitem__(self, index):
         image_path = self.path + '/' + os.listdir(self.path)[index]
-        x = ToTensor()(Image.open(image_path))
-        F.interpolate(x, 10000)
+        x = Image.open(image_path)
         if self.transform is not None:
             x = self.transform(x)
         return x, x
 
     def __len__(self):
-        return len(self.path)
+        return len(os.listdir(self.path))
 
 def layer_size(i: int) -> int:
-    in_size = 10000
-    encoder_size = 1000
+    in_size = 1000
+    encoder_size = 100
 
     encoder_dist = (10 - i)
     if encoder_dist < 0:
@@ -53,17 +56,21 @@ class Autoencoder(nn.Module):
             layers.append(nn.Linear(layer_size(i), layer_size(i + 1)))
             layers.append(nn.ReLU())
         self.linear_relu_stack = nn.Sequential(*layers)
+        self.loss_fn = nn.MSELoss()
 
     def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
-        return logits
+        return self.linear_relu_stack(x)
 
     def train(self, data):
-        loss_fn = nn.MSELoss()
         optimizer = torch.optim.SGD(self.parameters(), lr=1e-3)
 
         for i, (X, _) in enumerate(data):
+            input_size = layer_size(0)
+            X = transforms.Compose([
+                transforms.Resize((input_size, input_size), transforms.InterpolationMode.BILINEAR),
+                transforms.ToTensor(),
+            ])(X)
+
             Y = self(X)
             loss = self.loss_fn(Y, X)
 
@@ -74,21 +81,59 @@ class Autoencoder(nn.Module):
             if i % 100 == 0:
                 print("loss:", loss.item(), "; step:", i, "/", len(data))
 
-model = Autoencoder().to(device)
-print(model)
+    def test(self, data):
+        size = len(data)
+        total_loss = 0.
 
-data = ImageDataset(path='data/crowdsource_images-00000-of-00010/')
-data_loader = torch.utils.data.DataLoader(dataset=data, batch_size=32, shuffle=True, num_workers=4)
+        for i, (X, _) in enumerate(data):
+            input_size = layer_size(0)
+            X = transforms.Compose([
+                transforms.Resize((input_size, input_size), transforms.InterpolationMode.BILINEAR),
+                transforms.ToTensor(),
+            ])(X)
 
-#i = 0
-#fig = plt.figure(figsize=(8, 8))
-#for x, _ in data:
-#    fig.add_subplot(5, 5, i + 1)
-#    plt.imshow(x)
-#
-#    i += 1
-#    if i >= 25:
-#        break
-#plt.show()
+            Y = self(X)
+            total_loss += self.loss_fn(Y, X).item()
 
-model.train(data)
+        print("Average loss:", total_loss / size)
+
+if sys.argv[1] == '--train':
+    model = Autoencoder().to(device)
+    print(model)
+
+    data = ImageDataset(path='data/test/')
+    data_loader = torch.utils.data.DataLoader(dataset=data, batch_size=32, shuffle=True, num_workers=8)
+
+    epochs = 5
+    for i in range(epochs):
+        print("Epoch: ", i, "/", epochs)
+        model.train(data)
+        model.test(data)
+    print("Done!")
+
+    torch.save(model, 'model.pt')
+else:
+    model = torch.load('model.pt')
+    print(model)
+
+    image_path = sys.argv[1]
+    image = Image.open(image_path)
+    image_width, image_height = image.size
+    input_size = layer_size(0)
+    X = transforms.Compose([
+        transforms.Resize((input_size, input_size), transforms.InterpolationMode.BILINEAR),
+        transforms.ToTensor(),
+    ])(image)
+
+    Y = model(X)
+    Y = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.Resize((image_width, image_height), transforms.InterpolationMode.BILINEAR),
+    ])(Y)
+
+    fig = plt.figure(figsize=(8, 8))
+    fig.add_subplot(1, 2, 1)
+    plt.imshow(image)
+    fig.add_subplot(1, 2, 2)
+    plt.imshow(Y)
+    plt.show()
