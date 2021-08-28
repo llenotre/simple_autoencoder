db6d86d8cecc18eb3e435a117f78229285ab1456
llenotre <llenotre@student.42.fr>
2021-08-28 16:21:35 +0200

    Fix

--- main.py
+++ main.py
@@ -22,6 +22,12 @@ from os import listdir
 device = "cpu"
 print("Using {} device".format(device))
 
+
+
+dim = 90
+
+
+
 class ImageDataset(Dataset):
     def __init__(self, path, transform=None):
         self.path = path
@@ -38,48 +44,57 @@ class ImageDataset(Dataset):
         return len(os.listdir(self.path))
 
 class Autoencoder(nn.Module):
-    def __init__(self, in_size: int, out_size: int):
+    def __init__(self):
         super(Autoencoder, self).__init__()
 
-        self.in_size = in_size
-        self.out_size = out_size
-        self.encoder_depth = 5
+        self.encode_conv0 = nn.Conv2d(3, 6, kernel_size=3)
+        self.encode_relu0 = nn.ReLU()
+        self.encode_pool0 = nn.MaxPool2d(2, return_indices=True)
 
-        encode_layers = []
-        for i in range(0, self.encoder_depth):
-            encode_layers.append(nn.Linear(self.layer_size(i), self.layer_size(i + 1)))
-            encode_layers.append(nn.ReLU())
-        self.encoder = nn.Sequential(*encode_layers)
+        self.encode_conv1 = nn.Conv2d(6, 16, kernel_size=3)
+        self.encode_relu1 = nn.ReLU()
+        self.encode_pool1 = nn.MaxPool2d(2, return_indices=True)
 
-        decode_layers = []
-        for i in range(self.encoder_depth, self.encoder_depth * 2):
-            decode_layers.append(nn.Linear(self.layer_size(i), self.layer_size(i + 1)))
-            decode_layers.append(nn.ReLU())
-        self.decoder = nn.Sequential(*decode_layers)
+        self.decode_unpool0 = nn.MaxUnpool2d(2)
+        self.decode_conv0 = nn.ConvTranspose2d(16, 6, kernel_size=3)
+        self.decode_relu0 = nn.ReLU()
+
+        self.decode_unpool1 = nn.MaxUnpool2d(2)
+        self.decode_conv1 = nn.ConvTranspose2d(6, 3, kernel_size=3)
+        self.decode_relu1 = nn.ReLU()
 
         self.loss_fn = nn.MSELoss()
 
-    def layer_size(self, i: int) -> int:
-        slope = (self.in_size - self.out_size) / self.encoder_depth
-        encoder_dist = abs(self.encoder_depth - i)
-        return int(self.out_size + encoder_dist * slope)
+    def encode(self, x):
+        x = self.encode_conv0(x)
+        x = self.encode_relu0(x)
+        x, indices0 = self.encode_pool0(x)
 
-    def get_input_size(self):
-        return self.in_size
+        x = self.encode_conv1(x)
+        x = self.encode_relu1(x)
+        x, indices1 = self.encode_pool1(x)
+        return x, indices0, indices1
 
-    def get_output_size(self):
-        return self.out_size
+    def decode(self, x, indices0, indices1):
+        x = self.decode_unpool0(x, indices1)
+        x = self.decode_conv0(x)
+        x = self.decode_relu0(x)
+
+        x = self.decode_unpool1(x, indices0)
+        x = self.decode_conv1(x)
+        x = self.decode_relu1(x)
+        return x
 
     def forward(self, x):
-        return self.decoder(self.encoder(x))
+        x, indices0, indices1 = self.encode(x)
+        return self.decode(x, indices0, indices1)
 
     def do_train(self, data):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-4)
 
         self.train()
         for i, (X, _) in enumerate(data):
-            X = X.to(device)
-            X = torch.flatten(X)
+            X = X.view(1, 3, dim, dim).to(device)
             Y = self(X)
             loss = self.loss_fn(Y, X)
 
@@ -97,8 +112,7 @@ class Autoencoder(nn.Module):
         self.eval()
         with torch.no_grad():
             for i, (X, _) in enumerate(data):
-                X = X.to(device)
-                X = torch.flatten(X)
+                X = X.view(1, 3, dim, dim).to(device)
                 Y = self(X)
                 total_loss += self.loss_fn(Y, X).item()
 
@@ -106,21 +120,17 @@ class Autoencoder(nn.Module):
 
 
 
-channels_count = 3
-in_dim = 30
-out_dim = 10
-
 if sys.argv[1] == '--train':
-    model = Autoencoder(in_dim * in_dim * channels_count, out_dim * out_dim * channels_count).to(device)
+    model = Autoencoder().to(device)
     print(model)
 
     data = ImageDataset(path='data/test/', transform = transforms.Compose([
-        transforms.Resize((in_dim, in_dim), transforms.InterpolationMode.BILINEAR),
+        transforms.Resize((dim, dim), transforms.InterpolationMode.BILINEAR),
         transforms.ToTensor(),
     ]))
     data_loader = torch.utils.data.DataLoader(dataset=data, batch_size=32, shuffle=True, num_workers=4)
 
-    epochs = 5
+    epochs = 10
     for i in range(epochs):
         print("Epoch: ", i, "/", epochs)
         model.do_train(data)
@@ -129,7 +139,7 @@ if sys.argv[1] == '--train':
 
     torch.save(model.state_dict(), 'model.pth')
 else:
-    model = Autoencoder(in_dim * in_dim * channels_count, out_dim * out_dim * channels_count)
+    model = Autoencoder()
     model.load_state_dict(torch.load('model.pth'))
     model.eval()
     print(model)
@@ -138,35 +148,23 @@ else:
     image = Image.open(image_path)
     image_width, image_height = image.size
     X = transforms.Compose([
-        transforms.Resize((in_dim, in_dim), transforms.InterpolationMode.BILINEAR),
+        transforms.Resize((dim, dim), transforms.InterpolationMode.BILINEAR),
         transforms.ToTensor(),
     ])(image)
     shape = X.shape
-    X = torch.flatten(X)
 
     with torch.no_grad():
-        X = X.to(device)
-        Y = model.encoder(X)
-    Y = Y.view((channels_count, out_dim, out_dim))
+        X = X.view(1, 3, dim, dim).to(device)
+        Y = model(X)
+    Y = Y.view(shape)
     Y = transforms.Compose([
         transforms.ToPILImage(),
-        transforms.Resize((image_height, image_width), transforms.InterpolationMode.BILINEAR),
+        transforms.Resize((dim, dim), transforms.InterpolationMode.BILINEAR),
     ])(Y)
 
-    with torch.no_grad():
-        X = X.to(device)
-        decoded = model(X)
-    decoded = decoded.view(shape)
-    decoded = transforms.Compose([
-        transforms.ToPILImage(),
-        transforms.Resize((image_height, image_width), transforms.InterpolationMode.BILINEAR),
-    ])(decoded)
-
     fig = plt.figure(figsize=(8, 8))
-    fig.add_subplot(1, 3, 1)
+    fig.add_subplot(1, 2, 1)
     plt.imshow(image)
-    fig.add_subplot(1, 3, 2)
+    fig.add_subplot(1, 2, 2)
     plt.imshow(Y)
-    fig.add_subplot(1, 3, 3)
-    plt.imshow(decoded)
     plt.show()
